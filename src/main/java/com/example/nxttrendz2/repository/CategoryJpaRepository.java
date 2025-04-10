a907c60e85eb1609c21fc46c11944b61067caf18
Sravan <122646039+Sravan879@users.noreply.github.com>
2025-04-11 01:06:15 +0530

    delete comments

--- src/main/java/com/example/nxttrendz2/repository/CategoryJpaRepository.java
+++ src/main/java/com/example/nxttrendz2/repository/CategoryJpaRepository.java
@@ -1,13 +1,3 @@
-/*
- *
- * You can use the following import statements
- * 
- * import org.springframework.data.jpa.repository.JpaRepository;
- * import org.springframework.stereotype.Repository;
- * 
- */
-
-// Write your code here
 package com.example.nxttrendz2.repository;
 
 import com.example.nxttrendz2.model.Category;
