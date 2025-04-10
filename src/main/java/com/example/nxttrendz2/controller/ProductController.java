b9fa365ee43fc30c5c3bca80ff405bd54cdbfc05
Sravan <122646039+Sravan879@users.noreply.github.com>
2025-04-11 01:05:13 +0530

    removed comments

--- src/main/java/com/example/nxttrendz2/controller/ProductController.java
+++ src/main/java/com/example/nxttrendz2/controller/ProductController.java
@@ -1,14 +1,3 @@
-/*
- *
- * You can use the following import statements
- * 
- * import org.springframework.beans.factory.annotation.Autowired;
- * import org.springframework.web.bind.annotation.*;
- * import java.util.ArrayList;
- * 
- */
-
-// Write your code here
 package com.example.nxttrendz2.controller;
 
 import com.example.nxttrendz2.model.*;
